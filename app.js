b1034ac18071a49523316c2dfd31322eaa3aa820
myles-hash <mylesbillington@gmail.com>
2024-02-07 13:25:24 +0000

    git init

---
+++ app.js
@@ -0,0 +1,59 @@
+const result = document.getElementById("result");
+
+
+
+function go(){
+    let playerAnswerInput = document.getElementById("playerAnswer");
+    let playerAnswer = playerAnswerInput.value.toLowerCase();
+
+    if (playerAnswer !== "r" && playerAnswer !== "p" && playerAnswer !== "s") {
+        
+        alert("Please enter 'r', 'p', or 's' to choose rock paper or scissors!");
+        playerAnswerInput.value = "";
+        return;
+
+        
+    }
+    const playerAns = document.getElementById("playerAns");
+    playerAns.innerText = playerAnswer;
+
+    let computerResponseNum = getRandomInt(1, 3);
+    let computerResponseLetter = document.getElementById("computerAns");
+    
+
+    
+    
+
+
+    if (computerResponseNum === 1){
+         computerResponseLetter.innerText = "r"
+         
+    } else if (computerResponseNum === 2){
+         computerResponseLetter.innerText = "p"
+         
+    } else {
+         computerResponseLetter.innerText = "s"
+        
+    }
+
+
+    console.log(computerResponseLetter.innerText)
+    console.log(playerAnswer)
+
+    if (computerResponseLetter.innerText === playerAnswer){
+        result.innerText = "DRAW!"
+    }
+    else if (computerResponseLetter.innerText === "r" && playerAnswer === "s" || computerResponseLetter.innerText === "p" && playerAnswer === "r" || computerResponseLetter.innerText === "s" && playerAnswer === "p") {
+        result.innerText = "COMPUTER WINS :( !"
+    } else {
+        result.innerText = "YOU WIN :) !"
+    }
+
+    playerAnswerInput.value = "";
+    
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
