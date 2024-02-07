1922a4d0d34b9323811303d3690464f2906d1249
myles-hash <mylesbillington@gmail.com>
2024-02-07 14:58:36 +0000

    more css

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const result = document.getElementById("result");
+const result2 = document.getElementById("result2");
 
 
 
@@ -15,7 +16,15 @@ function go(){
         
     }
     const playerAns = document.getElementById("playerAns");
-    playerAns.innerText = playerAnswer;
+
+    if (playerAnswer === "r"){
+    playerAns.innerText = "Rock";
+    } else if (playerAnswer === "p"){
+        playerAns.innerText = "Paper";
+        }
+    else {
+        playerAns.innerText = "Scissors";
+    }
 
     let computerResponseNum = getRandomInt(1, 3);
     let computerResponseLetter = document.getElementById("computerAns");
@@ -26,25 +35,28 @@ function go(){
 
 
     if (computerResponseNum === 1){
-         computerResponseLetter.innerText = "r"
+         computerResponseLetter.innerText = "Rock"
          
     } else if (computerResponseNum === 2){
-         computerResponseLetter.innerText = "p"
+         computerResponseLetter.innerText = "Paper"
          
     } else {
-         computerResponseLetter.innerText = "s"
+         computerResponseLetter.innerText = "Scissors"
         
     }
 
 
 
-    if (computerResponseLetter.innerText === playerAnswer){
+    if (computerResponseLetter.innerText === playerAns.innerText){
         result.innerText = "DRAW!"
+        result2.innerText = "Too bad | Type and SHOOT to play again..."
     }
-    else if (computerResponseLetter.innerText === "r" && playerAnswer === "s" || computerResponseLetter.innerText === "p" && playerAnswer === "r" || computerResponseLetter.innerText === "s" && playerAnswer === "p") {
+    else if (computerResponseLetter.innerText === "Rock" && playerAns.innerText === "Scissors" || computerResponseLetter.innerText === "Paper" && playerAns.innerText === "Rock" || computerResponseLetter.innerText === "Scissors" && playerAnswer === "Paper") {
         result.innerText = "COMPUTER WINS :( "
+        result2.innerText = "Sucker | Type and SHOOT to play again..."
     } else {
         result.innerText = "YOU WIN :) "
+        result2.innerText = "Well done (you had a 33% chance) | Type and SHOOT to play again..."
     }
 
     playerAnswerInput.value = "";
